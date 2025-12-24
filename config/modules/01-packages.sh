a96f3c6a6cbfed6c8388b928c0f46141532d8cc5
Eric Rowan <ericrowan@icloud.com>
2025-12-24 17:06:28 -0500

    chore: rework welcome flow

--- config/modules/01-packages.sh
+++ config/modules/01-packages.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -ouex pipefail
 
-# 1. Decrapify
+echo "📦 Installing System Packages..."
+
+# 1. Decrapify (Remove stock bloat)
 rpm-ostree override remove \
     firefox firefox-langpacks \
     gnome-tour \
     gnome-software-rpm-ostree \
-    yelp
+    yelp \
+    gnome-user-docs
 
-# 2. Install System Tools (Bake these in!)
+# 2. Install Core Tools (System Layers)
 rpm-ostree install \
     fish \
     just \
@@ -25,5 +28,12 @@ rpm-ostree install \
     cifs-utils \
     nfs-utils
 
-# 3. Starship Binary
+# 3. Install Starship (Shell Prompt)
 curl -sS https://starship.rs/install.sh | sh -s -- -y -b /usr/bin
+
+# 4. Install Gum (The TUI Builder) - ARM64 Manual Install
+# We fetch the binary directly to avoid repo missing errors
+echo "🍬 Installing Gum..."
+GUM_VERSION="0.13.0"
+curl -sL "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_linux_arm64.tar.gz" | tar xz -C /usr/bin --strip-components=1 "gum_${GUM_VERSION}_linux_arm64/gum"
+chmod +x /usr/bin/gum
